4c9e0cdcfd63a89139bbe2e9ea28963475dc5a93
Raheem Alibhai <ral@ivoireacademie.com>
2022-08-16 17:03:00 +0800

    feature : update finish

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,8 +12,32 @@ class ArticlesController < ApplicationController
     @articles = Article.new
   end
 
+  def create
+    @articles = Article.new(articles_params)
+    if @articles.save
+      redirect_to articles_path
+    else
+      redirect_to new_article_path
+    end
+  end
+
   def edit
     @articles = Article.find(params[:id])
   end
+
+  def update
+    @articles = Article.find(params[:id])
+    if @articles.update(articles_params)  # true
+      redirect_to articles_path(@articles)
+    else
+      redirect_to edit_article_path(@articles) # @loan is similar to do @loan.id
+    end
+  end
+
+private
+
+def articles_params
+  params.require(:article).permit(:title, :content)
+end
 
 end
